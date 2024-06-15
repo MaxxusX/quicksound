4af8ca38f5dc4ccfe20e31c2aeb3abdd6354c9c2
MaxxusX <66540504+MaxxusX@users.noreply.github.com>
2024-06-15 18:22:57 -0500

    brugh

--- site/debug.js
+++ site/debug.js
@@ -1,6 +1,6 @@
 // for when i don't have easy access to dev console
 if (document.querySelector("html").dataset.debug !== undefined) {
-  document.querySelector(".spread").textContent = "debug";
+  document.addEventListener("DOMContentLoaded", () => document.querySelector(".spread").textContent = "debug");
   
   const oldconsoledebug = (...t) => console.debug(t);
   console.debug = (...t) => {
